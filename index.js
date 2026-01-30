f909964641734f3e02d2fcde6f0ef7ec6d650da1
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-01-30 18:48:04 +0000

    cfgytdfgyuhb

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+let calculateTotal = function (a, b = 2.50){
+    return a + b;
+}
+let printTopThreeHeadlines = function (...value){
+    let headlines 
+}
 
+
+document.getElementById("result").innerHTML = calculateTotal;
+
+
+//should create a function named calculateTotal that takes two parameters subtotal
+//and shipping and returns the sum of both values
+
+//should set a default value for shipping to 2.50
+
+//should create a function named printTopThreeHeadlines that takes a rest parameter 
+//list of string headlines named headlines and returns the top three 
+//headlines with a new line between each headline
