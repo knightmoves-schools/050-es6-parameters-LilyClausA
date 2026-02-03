733d30ed8911df4758d5a03948c453de78a66c86
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-02-03 20:26:22 +0000

    sderfvtgbyhunji

--- index.js
+++ index.js
@@ -3,22 +3,28 @@ let calculateTotal = function (a, b = 2.50){
 }
 let printTopThreeHeadlines = function (...value){
 
-    return value[1];
-    return value[2];
-    return value[3];
+    return headlines.slice(0, 3).join("\n");
 
 }
 
-result = printTopThreeHeadlines(`first headline, second headline, third headline`)
+//result = printTopThreeHeadlines(`first headline, second headline, third headline`)
 
 
-document.getElementById("result").innerHTML = calculateTotal;
+document.getElementById("result").innerHTML = printTopThreeHeadlines;
 
 
 //    ✕ should create a function named printTopThreeHeadlines that takes a rest
 //  parameter list of string headlines named headlines and returns the top three
 //  headlines with a new line between each headline (127 ms)
 //
-//    TypeError: Cannot read properties of undefined (reading 'trim')
-
-//   expect(result.trim()).toBe(`first headline\nsecond headline\nthird headline`)
+//    expect(received).toBe(expected) // Object.is equality
+//  
+  //    - Expected  - 3
+    //  + Received  + 1
+  //
+    //  - first headline
+      //- second headline
+//      - third headline
+  //    + second headline
+      //
+     //  expect(result.trim()).toBe(`first headline\nsecond headline\nthird headline`)
